59b509c5eb95fd471bd3c2d7b9464204c3d5eaaa
50ur43h <92090159+50ur43h@users.noreply.github.com>
2025-04-04 14:42:51 +0530

    Add files via upload

---
+++ Assignment001.py
@@ -0,0 +1,22 @@
+
+#Task 1: Perform Basic Mathematical Operations
+
+a=int(input('\nEnter the First number: '))
+b=int(input('Enter the Second number: '))
+A=a+b
+S=a-b
+M=a*b
+D=float(a/b)
+print("\nAddition:",A)
+print("Subtraction:",S)
+print("Multiplication:",M)
+print("Division:",D)
+
+
+#Task 2: Create a Personalized Greeting
+
+fn=input('\n\nEnter your First name: ')
+ln=input('Enter your Last name: ')
+ln=' '+ln+'!'
+nm=fn+ln
+print('\nHello,',nm,'Welcome to the Python program.\n')
